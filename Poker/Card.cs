5b25da59d81d55998db2c37c14ac72b498d5a3f7
Borja Quevedo <borjota@gmail.com>
2013-09-14 19:57:29 +0200

    Initial commit

---
+++ Poker/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public enum CardSuit
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    };
+
+    public class Card
+    {
+        public int Value { get; private set; }
+        public CardSuit Suit { get; private set; }
+
+        public Card(int value, CardSuit suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+}
---
+++ PokerTest/PokerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Poker;
+
+namespace PokerTest
+{
+    [TestFixture]
+    public class PokerTest
+    {
+        [Test(Description = "This test create a card and check its attributes")]
+        public void CreateAndCheckACard()
+        {
+            var card = new Card(1, CardSuit.Spades);
+            Assert.AreEqual(1, card.Value);
+            Assert.AreEqual(CardSuit.Spades, card.Suit);
+        }
+    }
+}
